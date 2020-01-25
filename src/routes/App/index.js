34be55d91b61e4f73de97170c55eed1f0914ef78
Fagner Gomes <fagner.egomes@gmail.com>
2020-01-25 18:28:05 -0300

    Created tab todo

--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createStackNavigator } from 'react-navigation-stack';
-
 import Icon from 'react-native-vector-icons/MaterialIcons'
+
 
 import Dashboard from "../../pages/Dashboard";
 import New from "../../pages/New";
+
 import Profile from "../../pages/Profile";
+import Todo from "../../pages/Todo";
 
 import commonStyles from '../../styles'
 
@@ -22,7 +24,13 @@ export const App = createBottomTabNavigator({
       tabBarIcon: ({ tintColor }) => <Icon name='poll' size={24} color={tintColor} />
     }
   },
-  Desejos,
+  Todo: {
+    screen: Todo,
+    navigationOptions: ({
+      title: 'Lista de Desejos',
+      tabBarIcon: ({ tintColor }) => <Icon name='local-atm' size={24} color={tintColor} />
+    })
+  },
   Profile: {
     screen: Profile
   }
